9288aee9057fd10dce77930fbf9bee8efdf41b46
Augusto Nodari <augustonodari@gmail.com>
2026-09-06 22:31:49 -0300

    create implementation of IStockPriceFetche YahooFinanceStockPriceFetch

---
+++ stock-quote-alert/YahooFinanceStockPriceFetch.cs
@@ -0,0 +1,9 @@
+﻿namespace stock_quote_alert;
+
+public class YahooFinanceStockPriceFetch: IStockPriceFetche
+{
+    decimal IStockPriceFetche.GetPrice(string symbol)
+    {
+        return 0;
+    }
+}
