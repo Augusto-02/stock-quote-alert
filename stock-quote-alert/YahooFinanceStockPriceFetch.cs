71365c80ad7f1a0a33930d26d0e8b7ff1d6d3425
Augusto Nodari <augustonodari@gmail.com>
2026-09-07 00:44:47 -0300

    add validations and http request to fetcher method

--- stock-quote-alert/YahooFinanceStockPriceFetch.cs
+++ stock-quote-alert/YahooFinanceStockPriceFetch.cs
@@ -1,9 +1,70 @@
 ﻿namespace stock_quote_alert;
 
-public class YahooFinanceStockPriceFetch: IStockPriceFetche
+using System.Net;
+
+public class YahooFinanceStockPriceFetcher : IStockPriceFetcher
 {
-    decimal IStockPriceFetche.GetPrice(string symbol)
+    private readonly HttpClient httpClient;
+    private const int MaxRetries = 3;
+
+    public YahooFinanceStockPriceFetcher(HttpClient httpClient)
     {
-        return 0;
+        this.httpClient = httpClient;
+    }
+
+    public async Task<decimal> GetPriceAsync(string symbol)
+    {
+        for (int index = 1; index <= MaxRetries; index++)
+        {
+            try
+            {
+                using HttpResponseMessage response = await httpClient.GetAsync(symbol);
+                int responseStatusCode = (int)response.StatusCode;
+                bool shouldRetry = responseStatusCode >= 500 || response.StatusCode == HttpStatusCode.TooManyRequests;
+                if (response.IsSuccessStatusCode)
+                {
+                    return 10;
+                }
+                else if (shouldRetry && index < MaxRetries)
+                {
+                    await Task.Delay(
+                        TimeSpan.FromSeconds(index));
+                    continue;
+                }
+                else if (shouldRetry && index == MaxRetries)
+                {
+                    throw new HttpRequestException($"\"All retries failed while getting ticker {symbol}.");
+                }
+                else if (!shouldRetry)
+                {
+                    throw new InvalidOperationException(
+                        $"API ERROR on getting ticker: {symbol} status code {response.StatusCode}: {response.ReasonPhrase ?? "Not informed"}.");
+                }
+            }
+            catch (HttpRequestException error)
+            {
+                if (index == MaxRetries)
+                {
+                    throw new HttpRequestException(
+                        $"All retries failed while getting ticker: {symbol}.",
+                        error);
+                }
+                await Task.Delay(
+                    TimeSpan.FromSeconds(index));
+            }
+            catch (TaskCanceledException error)
+            {
+                if (index == MaxRetries)
+                {
+                    throw new HttpRequestException(
+                        $"All retries failed while getting ticker: {symbol}.",
+                        error);
+                }
+                await Task.Delay(TimeSpan.FromSeconds(index));
+            }
+        }
+
+
+        throw new HttpRequestException($"\"All retries failed while getting ticker {symbol}.");
     }
 }
