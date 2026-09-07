38445253ab590658d42f3e9224add8f03793c756
Augusto Nodari <augustonodari@gmail.com>
2026-09-07 01:25:16 -0300

    add httpClient and injection dependency

--- stock-quote-alert/Program.cs
+++ stock-quote-alert/Program.cs
@@ -1,8 +1,23 @@
-﻿
+﻿namespace stock_quote_alert;
+
+using stock_quote_alert.Config;
+using System.Net.Http;
+
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
+        ApiConfig enviromentVariables;
+        try
+        {
+            enviromentVariables = EnvironmentSettingsLoader.LoadApiSettings();
+        }
+        catch (InvalidOperationException error)
+        {
+            Console.WriteLine(error.Message);
+            return;
+        }
+
         string request = Console.ReadLine();
         List<StockPriceAlert> requestData = new List<StockPriceAlert>();
         try
@@ -15,6 +30,15 @@ class Program
             return;
         }
 
-        Console.WriteLine(requestData);
+        HttpClient httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(enviromentVariables.PriceApiUrl),
+            Timeout = TimeSpan.FromSeconds(30)
+        };
+        IStockPriceFetcher fetcher =
+            new YahooFinanceStockPriceFetcher(httpClient);
+        StockPriceAlertService service =
+            new StockPriceAlertService(fetcher);
+        await service.MonitorAlertsAsync(requestData);
     }
 }
