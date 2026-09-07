d29b8a76bed0f3ffe8caa9dacd6ea12c3e0a3620
Augusto Nodari <augustonodari@gmail.com>
2026-09-07 01:25:47 -0300

    add api config

---
+++ stock-quote-alert/Config/ApiConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace stock_quote_alert.Config;
+
+public class ApiConfig
+{
+    public required string PriceApiUrl { get; init; }
+    public required int PollingIntervalSeconds { get; init; } = 30;
+}
