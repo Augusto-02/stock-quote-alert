aebb060959b2e52fca4d231b65aeedd5983faf69
Augusto Nodari <augustonodari@gmail.com>
2026-09-07 23:59:30 -0300

    add ApiToken to api config

--- stock-quote-alert/Config/ApiConfig.cs
+++ stock-quote-alert/Config/ApiConfig.cs
@@ -4,4 +4,6 @@ public class ApiConfig
 {
     public required string PriceApiUrl { get; init; }
     public required int PollingIntervalSeconds { get; init; } = 30;
+    
+    public required string ApiToken { get; init; }
 }
