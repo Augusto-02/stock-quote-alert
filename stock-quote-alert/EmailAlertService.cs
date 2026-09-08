cabae22cc4bd177a9b8f8adf709d2bffa508448d
Augusto Nodari <augustonodari@gmail.com>
2026-09-08 01:51:37 -0300

    add AlertInterface e EmailAlertService

---
+++ stock-quote-alert/EmailAlertService.cs
@@ -0,0 +1,42 @@
+﻿namespace stock_quote_alert;
+using stock_quote_alert.Config;
+using System.Net;
+using System.Net.Mail;
+
+
+public class EmailAlertService : IAlertService
+{
+    private readonly SmtpSettings _settings;
+
+    public EmailAlertService(SmtpSettings settings)
+    {
+        _settings = settings;
+    }
+
+    public async Task SendAlertAsync(
+        string ticker,
+        decimal actualPrice,
+        string alertType)
+    {
+        using MailMessage message = new(
+            _settings.From,
+            _settings.To);
+
+        message.Subject = $"Alerta de {alertType}: {ticker}";
+        message.Body =
+            $"Ticker: {ticker}\n" +
+            $"Preço atual: {actualPrice}\n" +
+            $"Tipo: {alertType}";
+
+        using SmtpClient smtpClient = new(
+            _settings.Host,
+            _settings.Port);
+
+        smtpClient.EnableSsl = true;
+        smtpClient.Credentials = new NetworkCredential(
+            _settings.Username,
+            _settings.Password);
+
+        await smtpClient.SendMailAsync(message);
+    }
+}
---
+++ stock-quote-alert/IAlertService.cs
@@ -0,0 +1,9 @@
+﻿namespace stock_quote_alert;
+
+public interface IAlertService
+{
+    Task SendAlertAsync(
+        string ticker,
+        decimal actualPrice,
+        string alertType);
+}
