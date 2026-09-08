d8d22fd9880bb4d07e1f7477d5ad1b5607ed8600
Augusto Nodari <augustonodari@gmail.com>
2026-09-08 01:51:05 -0300

    add SmtpSettings

---
+++ stock-quote-alert/Config/SmtpSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace stock_quote_alert.Config;
+
+public class SmtpSettings
+{
+    public required string Host { get; init; }
+    public required int Port { get; init; }
+    public required string Username { get; init; }
+    public required string Password { get; init; }
+    public required string From { get; init; }
+    public required string To { get; init; }
+}
